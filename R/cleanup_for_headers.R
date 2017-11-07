b8763821798f3f2ea745b16440789eea35c4ff53
Byers <nbyers1@iuhealth.org>
2017-11-07 12:31:02 -0500

    cleanup_for_headers

---
+++ R/cleanup_for_headers.R
@@ -0,0 +1,13 @@
+#' Transform vector into strings that can be headers in a data.frame
+#' @export
+cleanup_for_headers <- function(x) {
+
+  x <- trimws(x)
+  x <- gsub("&", "and", x)
+  x <- gsub("\\s", "_", x)
+  x <- gsub("\\/", "_", x)
+  x <- gsub("-", "_", x)
+  x <- gsub(";", "_", x)
+
+  x
+}
