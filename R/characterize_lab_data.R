1c84b91b6a93cfa4d84de9a6033b9a3fad1d4e10
Byers <nbyers1@iuhealth.org>
2018-02-26 11:08:38 -0500

    fixed

---
+++ R/characterize_lab_data.R
@@ -0,0 +1,12 @@
+
+characterize_lab_data <- function(lab_data, 
+                                  time_index_column = "time_index",
+                                  test_name_column = "test_name",
+                                  year_column = "year",
+                                  by_columne = "month",
+                                  variable_column = "variable",
+                                  value_column = "value") {
+  # lab_data <- read.csv("C:/Users/nbyers1/Documents/Repositories/Lab_Budget/simulate/simulated_lab_data.csv", stringsAsFactors = FALSE)
+  # time_index_column = "time_index"; test_name_column = "test_name"; year_column = "year"; by_columne = "month"; variable_column = "variable"; value_column = "value"
+  
+}
--- R/simulate_lab_data.R
+++ R/simulate_lab_data.R
@@ -1,6 +1,8 @@
 #' Generate data to test lab budget project
+#' @import tidyr
 #' @export
 #' @import parallel
+#' @param test_name Name for this test
 #' @param years How many years of data
 #' @param by What time duration do you want
 #' @param mean_volume What is the mean volume for this lab test
@@ -9,12 +11,13 @@
 #' @param seasonality Do you want seasonality during the year
 #' @param predictor_variables How many predictor variables do you want
 #' @param noisy_variables How many non-predictive variables do you want
+#' @param shape "long" means the variables will be gathered
 #' @examples 
 #' library(dplyr)
 #' library(tidyr)
 #' library(ggplot2)
 #' 
-#' x <- generate_lab_data(years = 3, by = "week", mean_volume = 100, 
+#' x <- simulate_lab_data(test_name = "a", years = 3, by = "week", mean_volume = 100, 
 #'                        sd_volume = 20, trend = TRUE, seasonality = TRUE, 
 #'                        predictor_variables = 2, noisy_variables = 1) 
 #' 
@@ -25,12 +28,13 @@
 #' 
 #' x <- tidyr::gather(x, key = "key", value = "value", test_volume:noisy1)
 #' 
-#' ggplot(x, aes(index, value, color = key)) + geom_point()
-generate_lab_data <- function(years, by = c("day", "week", "month", "year"), 
+#' ggplot(x, aes(time_index, value, color = key)) + geom_point()
+simulate_lab_data <- function(test_name, years, by = c("day", "week", "month", "year"), 
                               mean_volume = 100, sd_volume = 20, 
                               trend = FALSE, seasonality = FALSE, 
-                              predictor_variables = 0, noisy_variables = 0) {
-  # years = 3; by = "week"; mean_volume = 100; sd_volume = 20; trend = TRUE; seasonality = TRUE;  predictor_variables = 2; noisy_variables = 0
+                              predictor_variables = 0, noisy_variables = 0,
+                              shape = c("wide", "long")) {
+  # test_name = "a"; years = 3; by = "month"; mean_volume = 100; sd_volume = 20; trend = TRUE; seasonality = TRUE;  predictor_variables = 2; noisy_variables = 1; shape = "long"
   
   points_per_year <- c("day" = 365, "week" = 52, "month" = 12, "year" = 1)
   points_per_year <- points_per_year[names(points_per_year) == by]
@@ -48,8 +52,6 @@ generate_lab_data <- function(years, by = c("day", "week", "month", "year"),
   if(seasonality) {
     if(by == "year") stop("can't have seasonality with yearly numbers")
     
-    
-    
     season_wave <- cos(seq(0, 2*pi, length.out = points_per_year)) * 2*sd_volume
     
     lab_data <- lab_data + season_wave 
@@ -58,7 +60,8 @@ generate_lab_data <- function(years, by = c("day", "week", "month", "year"),
   lab_data <- round(lab_data)
   lab_data[lab_data < 0] <- 0
   
-  lab_data_frame <- data.frame(index = 1:n, year = 1:years, 
+  lab_data_frame <- data.frame(time_index = 1:n, test_name = test_name, 
+                               year = rep(1:years, each = points_per_year), 
                                by = rep(1:points_per_year, years),
                                test_volume = lab_data, 
                                stringsAsFactors = FALSE) 
@@ -89,6 +92,24 @@ generate_lab_data <- function(years, by = c("day", "week", "month", "year"),
       lab_data_frame[[paste0("noisy", j)]] <- rnorm(n = n, mean = mean_volume,
                                                     sd = sd_volume)
     }
+  }
+  
+  attr(lab_data_frame, "class") <- c("lab_data", "data.frame")
+  attr(lab_data_frame, "years") <- unname(years)
+  attr(lab_data_frame, "by") <- by
+  attr(lab_data_frame, "points_per_year") <- unname(points_per_year)
+  
+  if(shape[1] == "long" & predictor_variables + noisy_variables > 0) {
+    gather_columns <- "test_volume"
+    if(predictor_variables > 0) {
+      gather_columns <- c(gather_columns, paste0("predictor", 1:predictor_variables))
+    }
+    if(noisy_variables > 0) {
+      gather_columns <- c(gather_columns, paste0("noisy", 1:noisy_variables))
+    }
+    lab_data_frame <- tidyr::gather(lab_data_frame, "variable", "value",
+                                    !!gather_columns) %>%
+      arrange(time_index, desc(variable))
   }
   
   lab_data_frame
