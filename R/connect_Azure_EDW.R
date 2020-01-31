e85aca613f35ba865b81a8b28d5f7136a7a4adf6
Byers <nbyers1@iuhealth.org>
2020-01-30 20:03:44 -0500

    fix connect azure edw

--- R/connect_Azure_EDW.R
+++ R/connect_Azure_EDW.R
@@ -2,23 +2,34 @@
 #' @importFrom odbc odbc
 #' @export
 connect_Azure_EDW <- function(DSN = "Azure") {
-  con <- try(DBI::dbConnect(odbc::odbc(), DSN))
   
-  if(class(con) == "try-error") {
-    DBI_packages <- installed.packages() %>%
-      as.data.frame() %>%
-      dplyr::filter(Package == "DBI")
-    
-    for(i in row.names(DBI_packages)) {
-      
-      library_ <- as.character(DBI_packages[i, "LibPath"])
-      
-      remove.packages("DBI", lib = library_)
-      
-      install.packages("DBI", lib = library_)
-    }
+  con <- try(DBI::dbConnect(odbc::odbc(), DSN), silent = TRUE)
+  cnt <- 0
+  
+  while(inherits(con, 'try-error') && cnt < 3) {
+    cnt <- cnt + 1
+    reload_dbi()
+    con <- try(DBI::dbConnect(odbc::odbc(), DSN), silent = TRUE)
   }
   
-  DBI::dbConnect(odbc::odbc(), DSN)
+  if(inherits(con, 'try-error')) {
+    stop("Unable to reinstall DBI. Aborting", call. = FALSE)
+  } else {
+    return(con)
+  }
+
+}
+
+reload_dbi <- function() {
+  local({
+    library(IUHhelpers)
+    library(odbc)
+    library(DBI)
+    
+    detach("package:IUHhelpers", unload = TRUE, character.only = TRUE)
+    detach("package:odbc", unload = TRUE, character.only = TRUE)
+    detach("package:DBI", unload = TRUE, character.only = TRUE)
+    install.packages("DBI")
+  }, envir = .GlobalEnv)
   
 }
